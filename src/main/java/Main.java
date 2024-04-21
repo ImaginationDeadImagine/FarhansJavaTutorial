092b755bcb4913264c37e4452943347b9b97d2be
ImaginationDeadImagine <m.godotishere@gmail.com>
2024-04-21 08:53:49 +0000

    Type conversion: integer to double ok Type conversion: double to integer error: incompatible types: possible lossy conversion from double to int

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,12 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
 public class Main {
   public static void main(String[] args) {
-    // Two main types of data in Java:
-    // 1. Primitive data types - store values: int, double, boolean, char
-    // 2. Reference data types - store references to memorey locations while a dynamic object is being stored: String, Arrays, Objects
+    // Type conversion: double to integer
     
-    // Integer types
-    byte aSingleByte = 100;  // -128 to 127
-    short aSmallNumber = 20000;  // -32,768 to 32,767
-    int anInteger = 2147483647;  // -2,147,483,648 to 2,147,483,647
-    long aLargeNumber = 9223372036854775807L;  // 9223372036854775808 to -9223372036854775807
+    double number1 = 5.8;
+    int number2 = number1;
 
-    // Decimal types
-    double aDouble = 1.79769313;  // 4.9E-324 to 1.7976931348623157E308
-    float aFloat = 3.4028F;  // 1.4E-45 to 3.4028235E38
-
-    // booleans
-    boolean isWeekend = false;
-    boolean isWorkday = true;
-
-    // characters
-    char copyrightSymbol = '\u00A9';
-    
-    System.out.println("This is the copyright symbol: " + copyrightSymbol);
-    }
+    // Incompatible types: possible lossy conversion from double to int
+    System.out.println("Number 1 is " + number1);
+    System.out.println("Number 2 is " + number2);
+  }
 }
