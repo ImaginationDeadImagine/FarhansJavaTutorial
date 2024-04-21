29cf8c6182ef42ae343f792a9ab266370d9a264a
ImaginationDeadImagine <m.godotishere@gmail.com>
2024-04-21 06:56:38 +0000

    Declare and initialise variable

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int age;
+
+    age = 63;
+    
+    System.out.println("I am " + age + " years old.");
   }
 
   // @Test
