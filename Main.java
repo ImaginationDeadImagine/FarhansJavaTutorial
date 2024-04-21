3088ee03f58b544c7f538d90f6d39c9d0ca2f136
ImaginationDeadImagine <m.godotishere@gmail.com>
2024-04-21 07:08:59 +0000

    Class level, static variables

--- Main.java
+++ Main.java
@@ -3,10 +3,11 @@
 // import org.junit.jupiter.api.Test;
 
 public class Main {
+  // A static variable at the class level.
+  // NB A static method (like main here) can only work with static variables.
+  static int age = 63;
+  
   public static void main(String[] args) {
-    int age;
-
-    age = 63;
     
     System.out.println("I am " + age + " years old.");
   }
